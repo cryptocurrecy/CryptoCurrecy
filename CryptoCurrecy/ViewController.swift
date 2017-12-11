acfa1a6493303de7431e89c8414b5f699a3eb235
hi-ro-to <ichinose.higu@gmail.com>
2017-12-12 00:42:22 +0900

    first commit

---
+++ CryptoCurrecy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptoCurrecy
+//
+//  Created by Hiroto on 2017/12/12.
+//  Copyright © 2017年 hi-ro-to. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
